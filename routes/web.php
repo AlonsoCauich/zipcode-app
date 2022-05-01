fef4579f68ff63ce80d7eb42f1e3ae5340e0c98b
Alonso Cauich Viana <alonsocauichv1@gmail.com>
2022-05-01 04:06:29 -0500

    Change in resource

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,21 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+Route::get('/', function () use ($router) {
+    return response()->json([
+        "Modulo" => [
+            "Nombre" => "Reto Tecnico",
+            "Versión" => "1.0",
+            "Autor" => "Alonso de Jesus Cauich Viana",
+            "Email" => "alonsocauichv1@gmail.com",
+            "Telefono" => "9981892988",
+            "Github" => "https://github.com/AlonsoCauich/zipcode-app",
+            "Api" => "http://challenge.alonsocv.com/api/zip-codes/{zip_code}",
+        ]
+    ]);
 });
